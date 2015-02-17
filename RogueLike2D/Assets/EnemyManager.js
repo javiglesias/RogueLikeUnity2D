97494fd029d5edd51dc82a8588cdf8734b7fd4ce
javiglesias <javiglesias18@gmail.com>
2015-02-17 20:47:58 +0100

    Intento de spawn enemies
    Estoy intentado spawnear enemigos aleatoriamente por todo el mapa pero
    me da indexoutofBounds....

---
+++ RogueLike2D/Assets/EnemyManager.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var enemy : GameObject[];
+var spawnTime : float = 3f;
+var spawnPoint : Transform[];
+
+
+function Start () 
+{
+	InvokeRepeating("Spawn",spawnTime,spawnTime);
+}
+
+function Spawn()
+{
+	var spawnPintIndex : int = Random.Range(0, spawnPoint.Length);
+	Instantiate(enemy[spawnPintIndex], transform.position, transform.rotation);
+}
