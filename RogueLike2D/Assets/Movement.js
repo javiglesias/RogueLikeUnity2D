3b058d5f80c4664944317f68e2f0f67fe1782967
javiglesias <javiglesias18@gmail.com>
2015-02-18 11:44:39 +0100

    Camara...
    Ahora la camara sigue al personaje todo el rato, ademas el pj esta
    centrado en la camara

--- RogueLike2D/Assets/Movement.js
+++ RogueLike2D/Assets/Movement.js
@@ -3,6 +3,13 @@
 	var speed : float = 1.0;
 
 function Update () {
-	var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0);
+	var hit1 : RaycastHit;
+	var move = Vector3(Input.GetAxis("Horizontal"), 10*Input.GetAxis("Vertical"),0);
 	transform.position += move * speed * Time.deltaTime;
+
+	if(Physics.Raycast(transform.position, -Vector3.up,hit1))
+	{
+		var objectForward : Vector3 = transform.TransformDirection(Vector3.forward);
+		transform.rotation = Quaternion.LookRotation(objectForward, hit1.normal);
+	}
 }
